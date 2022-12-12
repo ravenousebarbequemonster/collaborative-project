5de7047b707b2695cdefa279839b29aa7729f581
ravenousebarbequemonster <118221711+ravenousebarbequemonster@users.noreply.github.com>
2022-12-12 14:03:23 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,93 @@
-
+let mySprite = sprites.create(assets.image`angy boi`, SpriteKind.Player)
+let mySprite2 = sprites.create(img`
+    . . . . . . f f f f f . . . . . 
+    . . . . . f e e e e e f . . . . 
+    . . . . f e e d d d d d f . . . 
+    . . . f f e d f f d d f f f . . 
+    . . f d d e d d d d e e d d b . 
+    . f f f d e d d c d d d d c b . 
+    f d b f d e d d d f f 1 f d b . 
+    f d d f f e e d d f f f f b . . 
+    f f f e f f e e d d d d b . . . 
+    . . f e e e f e e f f f . . . . 
+    . f f f e e e e e e e f . . . . 
+    . f e f f f e e e e e e f . . . 
+    . f e f f f f f e e e e f f . . 
+    . f e f f f b b f e e f d b f . 
+    . f f f f b d d f e e f d d f . 
+    . . f f f f f f f f f f f f f . 
+    `, SpriteKind.Player)
+let mySprite3 = sprites.create(assets.image`thing`, SpriteKind.Player)
+let mySprite4 = sprites.create(assets.image`big boi`, SpriteKind.Player)
+let mySprite5 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . d d . . . . . . . 
+    . . . . . . d d d d . . . . . . 
+    . . . . . d d d d d . . . . . . 
+    . . . . d d d d d d . . . . . . 
+    . . . . d d d d d d d . . . . . 
+    . . . . d d d d d d d . . . . . 
+    . . . d d d d d d d d d . . . . 
+    . . . d d d d d d d d d . . . . 
+    . . . d d d d d d d d d . . . . 
+    . . . d d d d d d d d . . . . . 
+    . . . . d d d d d d . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile)
+let mySprite6 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile)
+let mySprite7 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile)
+let mySprite8 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile)
